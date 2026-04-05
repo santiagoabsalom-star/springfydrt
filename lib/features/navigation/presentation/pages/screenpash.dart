6e1a73f3a4b78b64296eeb3e17dd882c10100061
Ultragodop <ultragodop@gmail.com>
2026-04-05 20:23:47 -0300

    nao nao ya se pueden descargar cnaciones en distintas playlist

--- lib/features/navigation/presentation/pages/screenpash.dart
+++ lib/features/navigation/presentation/pages/screenpash.dart
@@ -94,7 +94,7 @@ class _SplashScreenState extends State<SplashScreen> {
         Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(builder: (_) => const LoginScreen()),
-              (route) => false, // Elimina todas las rutas anteriores
+              (route) => false,
         );
       }
     }
